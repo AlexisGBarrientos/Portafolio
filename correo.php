0479136b0fa6fdc8e734c42c1be1395ccc913e20
AlexisGBarrientos <alexisgbarrientos@gmail.com>
2020-05-04 19:52:31 -0300

    Diseño responsivo + Front-end

---
+++ correo.php
@@ -0,0 +1,11 @@
+<?php
+
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    if (isset($_POST)) {
+        if (condition) {
+            # code...
+        }
+    }
